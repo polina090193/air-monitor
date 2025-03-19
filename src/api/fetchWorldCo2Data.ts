8684215430e700e2c3a7d3a649ee7cfa6b70ce2c
Polina Guseva <polina_0901@mail.ru>
2025-03-19 20:49:53 +0100

    Connect to DB

--- src/api/fetchWorldCo2Data.ts
+++ src/api/fetchWorldCo2Data.ts
@@ -1,14 +1,15 @@
 export const fetchWorldCo2Data = async (): Promise<WorldCO2DataRow[]> => {
   try {
     // Original source: https://carbonmonitor.org/
-    const fetchedWorldData = await fetch('/world_data.json')
-  
-    if (fetchedWorldData.ok) {
-      return await fetchedWorldData.json();
-  
-    } else {
-      throw new Error(`Error fetching world data: ${fetchedWorldData.statusText}`);
-    }
+    const response = await fetch('https://h4nz7c0yxg.execute-api.eu-north-1.amazonaws.com/world-co2');
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    } 
+    
+    const data = await response.json();
+    return data
+    
   } catch (error) {
     throw new Error(`Error fetching world data: ${error}`);
   }
